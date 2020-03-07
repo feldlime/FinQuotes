44f207fc404bf9ae06a81038b627caf3415aa0bc
feldlime <feldlime@yandex.ru>
2020-03-07 19:14:37 +0300

    added main

--- project/app/factory.py
+++ project/app/factory.py
@@ -1,8 +1,12 @@
+from typing import Optional
+
 from flask import Flask
 
 
-def create_app(config: object) -> Flask:
+def create_app(config: Optional[object] = None) -> Flask:
     app = Flask(__name__)
-    app.config.from_object(config)
+
+    if config is not None:
+        app.config.from_object(config)
 
     return app
