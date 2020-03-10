8a633929ef464757b2280628033cadf028d3d58f
feldlime <feldlime@yandex.ru>
2020-03-10 20:03:14 +0300

    registered bueprint

--- project/app/factory.py
+++ project/app/factory.py
@@ -2,11 +2,15 @@ from typing import Optional
 
 from flask import Flask
 
+from .api import bp
+
 
 def create_app(config: Optional[object] = None) -> Flask:
     app = Flask(__name__)
 
     if config is not None:
         app.config.from_object(config)
+
+    app.register_blueprint(bp, url_prefix='/api')
 
     return app
