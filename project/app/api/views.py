18b68d56876d722e9f23eb840f9bd7e81c71277d
feldlime <feldlime@yandex.ru>
2020-03-12 22:33:58 +0300

    used bcs

--- project/app/api/views.py
+++ project/app/api/views.py
@@ -3,6 +3,7 @@ from flask import Response, Blueprint
 from project.http import ok, server_error, bad_request
 from project.services import (
     smart_lab_quote,
+    bcs_quote,
     TickerNotFoundError,
     PriceNotFoundError,
 )
@@ -15,14 +16,15 @@ def ping() -> Response:
     return ok(message='pong')
 
 
-@bp.route('/quote/<string:code>', methods=('GET',))
-def quote(code: str) -> Response:
+@bp.route('/quote/<string:ticker>', methods=('GET',))
+def quote(ticker: str) -> Response:
     try:
-        price = smart_lab_quote(code)
+        # price = smart_lab_quote(code)
+        price = bcs_quote(ticker)
         return ok(data={'price': price})
     except TickerNotFoundError as e:
         return bad_request(message=f'Ticker not found: {e!r}')
     except PriceNotFoundError as e:
         return server_error(message=f'Price cannot be found: {e!r}')
     except Exception as e:
-        return server_error(message=f'Some problems while extracting price: {e!r}')
+        return server_error(message=f'Some problems while getting price: {e!r}')
--- project/services/__init__.py
+++ project/services/__init__.py
@@ -1,3 +1,3 @@
 from .smart_lab import quote as smart_lab_quote
-
+from .bcs import quote as bcs_quote
 from .exceptions import TickerNotFoundError, PriceNotFoundError
