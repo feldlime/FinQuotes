8b9ad321cc59183f7ffc5ce8b3fa10d1607077f3
feldlime <feldlime@yandex.ru>
2020-03-19 10:22:45 +0300

    changed views

--- project/app/views.py
+++ project/app/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Response, Blueprint
+from aiohttp import hdrs, web
 import attr
 
 from project.exceptions import (
@@ -10,17 +10,23 @@ from project.exceptions import (
 from project.http import ok, server_error, bad_request
 from project.quotes import get_quote
 
+__all__ = (
+    'ROUTES',
+)
 
-bp = Blueprint('api', __name__)
 
-
-@bp.route('/ping', methods=('GET',))
-def ping() -> Response:
+async def ping(_: web.Request) -> web.Response:
     return ok(message='pong')
 
 
-@bp.route('/quote/<string:ticker>', methods=('GET',))
-def quote(ticker: str) -> Response:
+async def time(_: web.Request) -> web.Response:
+    now = datetime.now()
+    now_str = now.strftime('%Y-%m-%d %H:%M:%S')
+    return ok(data={'time': now_str})
+
+
+async def quote(request: web.Request) -> web.Response:
+    ticker = request.match_info['ticker']
     try:
         quote_ = get_quote(ticker)
         return ok(data=attr.asdict(quote_))
@@ -32,8 +38,8 @@ def quote(ticker: str) -> Response:
         return server_error(message=f'Some problems while getting price: {e!r}')
 
 
-@bp.route('/time', methods=('GET',))
-def time() -> Response:
-    now = datetime.now()
-    now_str = now.strftime('%Y-%m-%d %H:%M:%S')
-    return ok(data={'time': now_str})
+ROUTES = (
+    web.route(hdrs.METH_ANY, '/ping', ping, name='ping'),
+    web.route(hdrs.METH_ANY, '/time', time, name='time'),
+    web.route(hdrs.METH_GET, '/quote/{ticker}', quote, name='quote'),
+)
