4a8ace36f95120572f3bdd274727506940e8d40a
feldlime <feldlime@yandex.ru>
2020-03-26 11:07:28 +0300

    made moex async

--- project/services/moex.py
+++ project/services/moex.py
@@ -1,24 +1,28 @@
-import requests
+import aiohttp
 
 from project.exceptions import GettingQuoteError
 
 
-def fetch(engine: str, market: str, code: str) -> requests.Response:
-    base_url = 'https://iss.moex.com/iss'
-    url = f'{base_url}/engines/{engine}/markets/{market}/securities/{code}.json'
-    resp = requests.get(url)
-    return resp
+def make_url(engine: str, market: str, ticker: str) -> str:
+    base = 'https://iss.moex.com/iss'
+    return f'{base}/engines/{engine}/markets/{market}/securities/{ticker}.json'
 
 
-def get_price_data(engine: str, market: str, code: str) -> dict:
-    resp = fetch(engine, market, code)
-    json = resp.json()
-    data = json['marketdata']['data']
-    return data
+async def fetch(
+        session: aiohttp.ClientSession,
+        engine: str,
+        market: str,
+        ticker: str,
+) -> dict:
+    url = make_url(engine, market, ticker)
+    async with session.get(url) as resp:
+        json = await resp.json()
+        return json
 
 
-def share_quote(code: str) -> float:
-    price_data = get_price_data('stock', 'shares', code)
+async def share_quote(session: aiohttp.ClientSession, ticker: str) -> float:
+    data = await fetch(session, 'stock', 'shares', ticker)
+    price_data = data['marketdata']['data']
     price_data = list(filter(lambda l: l[1] in ('TQBR', 'TQTF'), price_data))
 
     if not price_data:
@@ -28,8 +32,9 @@ def share_quote(code: str) -> float:
     return float(price)
 
 
-def bound_quote(code: str) -> float:
-    price_data = get_price_data('stock', 'bonds', code)
+async def bound_quote(session: aiohttp.ClientSession, ticker: str) -> float:
+    data = await fetch(session, 'stock', 'bonds', ticker)
+    price_data = data['marketdata']['data']
 
     if not price_data:
         raise GettingQuoteError()
